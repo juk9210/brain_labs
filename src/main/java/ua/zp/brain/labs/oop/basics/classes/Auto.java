d66a5000c72d23d127ce7e7a383403e33d73587a
juk9210 <shah.juk92@gmail.com>
2019-04-28 22:04:44 +0300

    brain labs

---
+++ src/main/java/ua/zp/brain/labs/oop/basics/classes/Auto.java
@@ -0,0 +1,72 @@
+package ua.zp.brain.labs.oop.basics.classes;
+
+/**
+ *
+ */
+public class Auto {
+    public String typeOfTransport;
+    public String bodyType;
+    public String countryProducer;
+
+    public String getTypeOfTransport() {
+        return typeOfTransport;
+    }
+
+    public void setTypeOfTransport(String typeOfTransport) {
+        this.typeOfTransport = typeOfTransport;
+    }
+
+    public String getBodyType() {
+        return bodyType;
+    }
+
+    public void setBodyType(String bodyType) {
+        this.bodyType = bodyType;
+    }
+
+    public String getCountryProducer() {
+        return countryProducer;
+    }
+
+    public void setCountryProducer(String countryProducer) {
+        this.countryProducer = countryProducer;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getYearOfRelease() {
+        return yearOfRelease;
+    }
+
+    public void setYearOfRelease(int yearOfRelease) {
+        this.yearOfRelease = yearOfRelease;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String brand;
+    public String model;
+    public int yearOfRelease;
+    public String color;
+    public static final String DRIVE_TYPE = "automaton";
+}
